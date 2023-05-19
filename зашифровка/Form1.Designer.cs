30a0d78eedc1ad4ac77decb98e2d999a447f04b6
RusONDes <129102059+RusONDes@users.noreply.github.com>
2023-05-19 22:15:57 +0300

    v1

---
+++ зашифровка/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace зашифровка
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(467, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "открыть и зашифровать ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(467, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "дешифровать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(434, 368);
+            this.listBox1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(467, 91);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(473, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ключь ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ зашифровка/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace зашифровка
+{
+    public partial class Form1 : Form
+    {
+        char[] str ;
+        char[] al = new char[25];
+        string ssss;
+        int gup = 0;
+        int num = 0;
+        int offset = 0;
+        int j = 0;
+        int key = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void list()
+        {
+           listBox1.Items.Clear();
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "что вы хотите рашифровать ";
+            openFileDialog1.Filter = "Все фаилы | *.*|Текстовые фаилы | *.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                list();
+                key = int.Parse(textBox1.Text);
+                string patc = openFileDialog1.FileName;
+                string text = File.ReadAllText(patc);
+                text = text.ToLower();
+                File.WriteAllText(patc, text);
+                str = text.ToCharArray();
+               
+                for (int i = 0; i < str.Length; i++)
+                {
+                   if (Char.IsWhiteSpace(str[i]))
+                    {
+                        str[i] = ' ';
+                    }
+                   else if (str[i] + key > 122)
+                    {
+                        offset = (key + str[i]) - 122;
+                        str[i] = (char)(96 + offset);
+                    }
+                   else
+                    {
+                        str[i] = (char)(str[i] + key);
+                    }
+                }
+                ssss = new string(str);
+                listBox1.Items.Add(ssss);
+            }
+            else
+            {
+                MessageBox.Show("вы закрыли окно ");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            key = int.Parse(textBox1.Text);
+            list();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsWhiteSpace(str[i]))
+                {
+                    str[i] = ' ';
+                }
+                else if (str[i] - key < 97)
+                {
+                    offset = 97 + (key - str[i]) ;
+                    str[i] = (char)(123 - offset);
+                }
+                else
+                {
+                    str[i] = (char)(str[i] - key);
+                }
+            }
+            ssss = new string(str);
+            listBox1.Items.Add(ssss);
+
+
+        }
+
+    }
+}
+
